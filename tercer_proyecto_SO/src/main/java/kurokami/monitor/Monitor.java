53a913544db2ec16235a4941c583c66c5c1ca913
Marcos <sierramarcos146@gmail.com>
2020-04-18 18:49:52 -0600

    Agregando clases monitor y boxeador

---
+++ tercer_proyecto_SO/src/main/java/kurokami/monitor/Monitor.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kurokami.monitor;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author marcos
+ */
+public class Monitor {
+    private ArrayList<Boxeador> pendientes;
+    private final int TOTAL_GUANTES;
+    private int numeroOponentes;
+    private int guantesDisponibles;
+            
+    public Monitor(int totalGuantes, int numeroOponentes){
+        this.TOTAL_GUANTES = totalGuantes;
+        this.guantesDisponibles = totalGuantes;
+        this.pendientes = new ArrayList();
+        this.numeroOponentes = numeroOponentes;
+    }
+
+    public ArrayList<Boxeador> getPendientes() {
+        return pendientes;
+    }
+
+    public int getTOTAL_GUANTES() {
+        return TOTAL_GUANTES;
+    }
+
+    public int getGuantesDisponibles() {
+        return guantesDisponibles;
+    }
+    public synchronized void devolverGuantes(){
+        this.guantesDisponibles += 2;
+        if (guantesDisponibles == 3 || this.guantesDisponibles == 2){
+            notify();
+        }
+    }
+    public synchronized Boxeador atenderBoxeador(){
+        while(this.guantesDisponibles < 2 || pendientes.isEmpty()){
+            try{
+                wait();
+            }catch(InterruptedException ex){}
+        }
+        this.guantesDisponibles -= 2;
+        Boxeador regresar = pendientes.remove(0);
+        return regresar;
+    }
+    public synchronized void agregarBoxeador(Boxeador nuevoBoxeador){
+        pendientes.add(nuevoBoxeador);
+        if(pendientes.size() == 1){
+            notify();
+        }
+    }
+}
