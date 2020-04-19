32014349e923a3c4f3bb3987479ea5f9695fb273
kheysser <kheysser@outlook.com>
2020-04-19 13:14:01 -0600

    Agregada clase boxeador

--- tercer_proyecto_SO/src/main/java/kurokami/monitor/Boxeador.java
+++ tercer_proyecto_SO/src/main/java/kurokami/monitor/Boxeador.java
@@ -9,6 +9,54 @@ package kurokami.monitor;
  *
  * @author marcos
  */
-public class Boxeador {
+public class Boxeador implements Runnable{
+     
+    private Monitor monitor;
+    private Thread hilo;
+    private int id;
+    private int guantesUsando;
+    private int tiempo;
+
+    public Boxeador(Monitor monitor, Thread hilo, int id, int guantesUsando, int tiempo) {
+        this.monitor = monitor;
+        this.hilo = hilo;
+        this.id = id;
+        this.guantesUsando = guantesUsando;
+        this.tiempo = tiempo;
+    }
+
+   
     
+    public synchronized void Consumir(){
+        
+         try{
+            System.out.println("Boxeador "+ id + " esta peleando");
+            int espera = (int)(Math.random()*1000);
+            System.out.println("El tiempo de boxeo es de: " + (espera/1000));
+            hilo.sleep(espera);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public synchronized void Esperar(){
+        try{
+            System.out.println("Boxeador "+ id + " esta esperando");
+            int espera = (int)(Math.random()*1000);
+            System.out.println("Tiempo de espera: " + (espera/1000));
+            hilo.sleep(espera);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }  
+ 
+    public void run(){
+        while(true){
+            monitor.agregarBoxeador();
+            Esperar();
+            monitor.atenderBoxeador();
+            Consumir();
+            monitor.devolverGuantes();
+        }
+    }
 }
